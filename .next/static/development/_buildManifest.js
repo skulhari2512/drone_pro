2d9a5b8ad844a09784b4fe39a9f7df3e2f40b095
Suman <skulhari2512@gmail.com>
2025-09-26 21:53:31 +0800

    bi

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -2,21 +2,6 @@ self.__BUILD_MANIFEST = {
   "/": [
     "./static/chunks/pages/index.js"
   ],
-  "/how-it-works": [
-    "./static/chunks/pages/how-it-works.js"
-  ],
-  "/industry-uses": [
-    "./static/chunks/pages/industry-uses.js"
-  ],
-  "/plan": [
-    "./static/chunks/pages/plan.js"
-  ],
-  "/quiz": [
-    "./static/chunks/pages/quiz.js"
-  ],
-  "/training": [
-    "./static/chunks/pages/training.js"
-  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [],
