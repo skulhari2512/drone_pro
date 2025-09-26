1e2d152ae76b11db6aa6fcbc922f3cf99657c753
Suman <skulhari2512@gmail.com>
2025-09-26 15:11:36 +0800

    c

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -2,6 +2,12 @@ self.__BUILD_MANIFEST = {
   "/": [
     "./static/chunks/pages/index.js"
   ],
+  "/_error": [
+    "./static/chunks/pages/_error.js"
+  ],
+  "/plan": [
+    "./static/chunks/pages/plan.js"
+  ],
   "/quiz": [
     "./static/chunks/pages/quiz.js"
   ],
