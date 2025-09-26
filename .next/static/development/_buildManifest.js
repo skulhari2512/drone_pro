2fbe24a0adf9888de675d27873b8094c8944bbba
Suman <skulhari2512@gmail.com>
2025-09-26 14:42:17 +0800

    no message

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -2,6 +2,12 @@ self.__BUILD_MANIFEST = {
   "/": [
     "./static/chunks/pages/index.js"
   ],
+  "/_error": [
+    "./static/chunks/pages/_error.js"
+  ],
+  "/quiz": [
+    "./static/chunks/pages/quiz.js"
+  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [],
